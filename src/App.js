fd1f3089da31a0a381b85e919a9deafb01bc6ccd
Renan Cavichi <renancavichi@gmail.com>
2022-03-28 17:21:08 -0300

    Alterei no git hub

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Meu Primeiro Projeto React
+          Alterei no github!
         </p>
         <a
           className="App-link"
