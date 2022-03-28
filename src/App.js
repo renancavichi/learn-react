f033f5f9ac20118c7c1ebbd5702ab669111bb584
Renan Cavichi <renancavichi@gmail.com>
2022-03-28 17:15:51 -0300

    Altera css e App js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
+          Meu Primeiro Projeto React
         </p>
         <a
           className="App-link"
