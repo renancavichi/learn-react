f128960c3d4d0bd2308c7432712db162aa8fdfd0
Renan Cavichi <renancavichi@gmail.com>
2022-03-28 17:54:10 -0300

    Em outra pasta

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Alterei no no vscode!
+          Alterei no em outra pasta!
         </p>
         <a
           className="App-link"
