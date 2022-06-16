8e5eedfdf0076226f13f196a93543bf5761bf16c
Renan Cavichi <renancavichi@gmail.com>
2022-06-16 01:51:57 -0300

    move theme and remove import React from test

--- src/providers/ColorModeProvider.tsx
+++ src/providers/ColorModeProvider.tsx
@@ -14,13 +14,11 @@ const ColorModeProvider = ({children}: Props) => {
 			if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
 				setMode('dark')
 			}
-
 			window
 				.matchMedia('(prefers-color-scheme: light)')
 				.addEventListener('change', event =>
 					setMode(event.matches ? 'light' : 'dark')
 				)
-
 			return () => {
 				window
 					.matchMedia('(prefers-color-scheme: light)')
