f3351e6f8c530d3f5020b992e6ae4f422c6a5e41
Renan Cavichi <renancavichi@gmail.com>
2022-10-25 17:48:03 -0300

    fix demo test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import {render, screen} from '@testing-library/react'
 import HomeGreetings from './components/sections/HomeGreetings'
 
-test('renders learn react link', () => {
+test('Render home with Renan Cavichi name', () => {
 	render(<HomeGreetings />)
 	const linkElement = screen.getByText(/Renan Cavichi/i)
 	expect(linkElement).toBeInTheDocument()
